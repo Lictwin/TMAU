3f37959391a45f2c7f5754708762580de91d2686
MaksimBatura <mbatura19@gmail.com>
2026-06-18 11:35:58 +0300

    удалил ненужные файлы

--- trunk/as057702/task_02/test/build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/googletest-1.14.0/googletest
-# Build directory: D:/TMAU/TMAU/trunk/as057702/task_02/test/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
